3fcd9a2e0341295b32f8a421551c33c00e1f8ef7
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-03-10 14:16:10 -0600

    added local .env and got local heroku working with it

--- dezine/settings.py
+++ dezine/settings.py
@@ -83,16 +83,15 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'tagazine',
-        'USER' : 'tagazine',
-        'PASSWORD' : 'tagazine',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
+        # 'NAME' : 'tagazine',
+        # 'USER' : 'tagazine',
+        # 'PASSWORD' : 'tagazine',
+        # 'HOST' : 'localhost',
+        # 'PORT' : '5432'
     }
 }
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
