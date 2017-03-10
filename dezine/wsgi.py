14c21585833d6813dec38ede42090c468ef75ddc
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-03-10 15:55:23 -0600

    refreshed req.txt

--- dezine/wsgi.py
+++ dezine/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-import django.core
+import django
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
