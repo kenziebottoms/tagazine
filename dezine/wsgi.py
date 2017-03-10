492ab523859f9809d95d5cb5a912615fd57fdd74
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-03-10 15:16:40 -0600

    reset wsgi

--- dezine/wsgi.py
+++ dezine/wsgi.py
@@ -1,8 +1,7 @@
+
 """
 WSGI config for dezine project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
@@ -10,9 +9,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dezine.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
