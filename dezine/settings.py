5a4e2a4422622fd85120ef8759b07a985e2d8454
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-29 10:44:15 -0500

    hard-coded db settings sorry

--- dezine/settings.py
+++ dezine/settings.py
@@ -81,18 +81,14 @@ WSGI_APPLICATION = 'dezine.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        # 'NAME' : 'tagazine',
-        # 'USER' : 'tagazine',
-        # 'PASSWORD' : 'tagazine',
-        # 'HOST' : 'localhost',
-        # 'PORT' : '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'tagazine',
+        'USER' : 'kenzie',
+        'PASSWORD' : 'kenzie',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
-
-DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
